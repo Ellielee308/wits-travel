66aeed0c28ba6e3f63495b22b8a0477a483680dd
Liang081821 <2001henry99@gmail.com>
2024-08-26 10:10:21 +0800

    fix : adjust style layout

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,13 +3,13 @@ export default function Footer() {
     <div className="flex h-[130px] w-full flex-col items-center justify-center border border-black text-lg md:fixed md:bottom-0 lg:md:text-xl">
       <div>李雨潔 姚乃文 林佳璇 吳芷吟 張量</div>
       <div className="flex items-center justify-center space-x-2 text-gray-800">
-        <span className="text-lg lg:md:text-xl">React</span>
+        <span className="text-lg lg:text-xl">React</span>
         <span className="mx-2 h-3 border border-black"></span>
-        <span className="text-lg lg:md:text-xl">Git</span>
+        <span className="text-lg lg:text-xl">Git</span>
         <span className="mx-2 h-3 border border-black"></span>
-        <span className="text-lg lg:md:text-xl">Tailwind</span>
+        <span className="text-lg lg:text-xl">Tailwind</span>
         <span className="mx-2 h-3 border border-black"></span>
-        <span className="text-lg lg:md:text-xl">Firebase</span>
+        <span className="text-lg lg:text-xl">Firebase</span>
       </div>
     </div>
   );
