3987cc0b25a5a008b3f21eae9f52b011a5f394dc
chris <pupu86019@gmail.com>
2024-08-26 17:30:54 +0800

    feat: integrate SpotsProvider to enable spot data fetching

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,21 @@ import Home from "./pages/Home";
 import Spot from "./pages/Spot";
 import ContactForm from "./pages/ContactForm";
 import "./index.css";
+import { SpotsProvider } from "./components/spotsContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Home />} />
-        <Route path="spot" element={<Spot />} />
-        <Route path="contacts" element={<ContactForm />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>,
+  <SpotsProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="spot" element={<Spot />} />
+          <Route path="contacts" element={<ContactForm />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </SpotsProvider>,
 );
