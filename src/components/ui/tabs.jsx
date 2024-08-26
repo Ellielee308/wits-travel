eb6faf0360a2f850ecd1b12bd6334832c561a4ce
Ellie Lee <as152808@gmail.com>
2024-08-26 15:01:04 +0800

    chore: props validation

--- src/components/ui/tabs.jsx
+++ src/components/ui/tabs.jsx
@@ -1,46 +1,29 @@
-import * as React from "react";
-import * as TabsPrimitive from "@radix-ui/react-tabs";
+import {
+  Tabs,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from "./YourComponentFilePath";
 
-import { cn } from "@/lib/utils";
+const MyTabs = () => {
+  return (
+    <Tabs defaultValue="tab1">
+      <TabsList className="my-tabs-list">
+        <TabsTrigger value="tab1">Tab 1</TabsTrigger>
+        <TabsTrigger value="tab2">Tab 2</TabsTrigger>
+        <TabsTrigger value="tab3">Tab 3</TabsTrigger>
+      </TabsList>
+      <TabsContent value="tab1" className="my-tabs-content">
+        Content for Tab 1
+      </TabsContent>
+      <TabsContent value="tab2" className="my-tabs-content">
+        Content for Tab 2
+      </TabsContent>
+      <TabsContent value="tab3" className="my-tabs-content">
+        Content for Tab 3
+      </TabsContent>
+    </Tabs>
+  );
+};
 
-const Tabs = TabsPrimitive.Root;
-
-const TabsList = React.forwardRef(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={cn(
-      //改灰底的背景
-      "inline-flex h-10 items-center justify-center rounded-md bg-stone-100 p-2 text-stone-500 dark:bg-stone-800 dark:text-stone-400 md:h-12",
-      className,
-    )}
-    {...props}
-  />
-));
-TabsList.displayName = TabsPrimitive.List.displayName;
-
-const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    ref={ref}
-    // data-[state=active]:bg-white改被點擊後的效果
-    className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-md px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-stone-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-stone-950 data-[state=active]:shadow-sm dark:ring-offset-stone-950 dark:focus-visible:ring-stone-300 dark:data-[state=active]:bg-stone-950 dark:data-[state=active]:text-stone-50 md:text-base",
-      className,
-    )}
-    {...props}
-  />
-));
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName;
-
-const TabsContent = React.forwardRef(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      "mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-stone-950 focus-visible:ring-offset-2 dark:ring-offset-stone-950 dark:focus-visible:ring-stone-300",
-      className,
-    )}
-    {...props}
-  />
-));
-TabsContent.displayName = TabsPrimitive.Content.displayName;
-
-export { Tabs, TabsList, TabsTrigger, TabsContent };
+export default MyTabs;
