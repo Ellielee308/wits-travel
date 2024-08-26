c132c7344903767717933916245c73ef5e54900c
Liang081821 <2001henry99@gmail.com>
2024-08-26 10:14:19 +0800

    fix: remove lg layout

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="flex h-[130px] w-full flex-col items-center justify-center border border-black text-lg md:fixed md:bottom-0 lg:md:text-xl">
+    <div className="flex h-[130px] w-full flex-col items-center justify-center border border-black text-lg md:fixed md:bottom-0 lg:text-xl">
       <div>李雨潔 姚乃文 林佳璇 吳芷吟 張量</div>
       <div className="flex items-center justify-center space-x-2 text-gray-800">
         <span className="text-lg lg:text-xl">React</span>
