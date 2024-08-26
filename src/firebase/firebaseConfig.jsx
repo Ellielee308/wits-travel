eb2d25df2b7cff9ab4b928eaa5d48a73e2dbe611
chris <pupu86019@gmail.com>
2024-08-26 17:49:02 +0800

    feat: fetch data from Firestore
    - Implemented fetchSpot function to retrieve data from Firestore
    - Configured Firebase in firebaseConfig.jsx

---
+++ src/firebase/firebaseConfig.jsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "witz-ec201.firebaseapp.com",
+  projectId: "witz-ec201",
+  storageBucket: "witz-ec201.appspot.com",
+  messagingSenderId: "777073842698",
+  appId: "1:777073842698:web:73f2a7cd7ba08a9b25ddf3",
+  measurementId: "G-VCKK3NXKM5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
