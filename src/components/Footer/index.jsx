26c1e6432f69768e89fccc44ec8899854e7e287a
Ellie Lee <as152808@gmail.com>
2024-08-26 11:25:07 +0800

    fix: layout details

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,11 +4,11 @@ export default function Footer() {
       <div>李雨潔 姚乃文 林佳璇 吳芷吟 張量</div>
       <div className="flex items-center justify-center space-x-2 text-gray-800">
         <span className="text-lg lg:text-xl">React</span>
-        <span className="mx-2 h-3 border border-black"></span>
+        <span className="mx-2 h-4 border-[0.5px] border-black"></span>
         <span className="text-lg lg:text-xl">Git</span>
-        <span className="mx-2 h-3 border border-black"></span>
+        <span className="mx-2 h-4 border-[0.5px] border-black"></span>
         <span className="text-lg lg:text-xl">Tailwind</span>
-        <span className="mx-2 h-3 border border-black"></span>
+        <span className="mx-2 h-4 border-[0.5px] border-black"></span>
         <span className="text-lg lg:text-xl">Firebase</span>
       </div>
     </div>
