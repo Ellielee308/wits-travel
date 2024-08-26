32436edee7213d2dbc2418bfe58ec995026355a8
chris <pupu86019@gmail.com>
2024-08-26 17:35:34 +0800

    feat: implement SpotsProvider to fetch data from Firestore
    - Created SpotsContext using createContext for managing spot data.
    - Implemented SpotsProvider component to fetch spot data from Firestore using fetchSpot function.
    - Integrated useEffect to fetch and set spot data on component mount.

---
+++ src/components/spotsContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState, useEffect } from "react";
+import { fetchSpot } from "@/firebase/fetchSpot";
+import PropTypes from "prop-types";
+
+export const SpotsContext = createContext();
+
+export const SpotsProvider = ({ children }) => {
+  const [spots, setSpots] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await fetchSpot();
+        console.log("Fetched data inside getData:", data);
+        setSpots(data);
+      } catch (error) {
+        console.error("Error fetching spots:", error);
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <SpotsContext.Provider value={spots}>{children}</SpotsContext.Provider>
+  );
+};
+
+SpotsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
