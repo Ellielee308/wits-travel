3d6f5764a84af8a880e4a1d6b9275c7543d34a1a
Serena <serenalin0420@gmail.com>
2024-08-27 22:00:31 +0800

    refactor: add link on category tag in spotList pages

--- src/pages/Home/SpotList.jsx
+++ src/pages/Home/SpotList.jsx
@@ -5,6 +5,13 @@ import { Link } from "react-router-dom";
 
 function SpotList() {
   const spots = useContext(SpotsContext);
+  const components = [
+    { spot_category: "歷史景點", href: "/category?category=history" },
+    { spot_category: "樂園", href: "/category?category=theme-park" },
+    { spot_category: "特色建築", href: "/category?category=architecture" },
+    { spot_category: "博物館 & 美術館", href: "/category?category=museum" },
+    { spot_category: "自然風景", href: "/category?category=nature" },
+  ];
 
   return (
     <div className="mx-6 my-8 lg:mx-8 xl:mx-auto xl:max-w-[1136px]">
@@ -53,53 +60,81 @@ function SpotList() {
         <TabsContent value="card" className="mt-3">
           <div className="grid grid-cols-2 gap-5 lg:grid-cols-4">
             {spots.map((spot, index) => (
-              <Link className="relative" key={index} to={`/spot?id=${spot.id}`}>
-                <div className="h-64 w-full overflow-hidden rounded-xl">
-                  <img
-                    src={spot.main_img}
-                    alt={spot.subtitle}
-                    className="h-full w-full transform object-cover transition-transform duration-500 ease-in-out hover:scale-110"
-                  />
-                  <div className="absolute bottom-0 left-0 mb-2 w-full rounded-xl bg-gradient-to-t from-slate-700/80 via-slate-700/40 to-transparent md:mb-0 md:h-2/6">
-                    <h2 className="z-10 mb-2 ml-4 mt-2 text-2xl text-white">
-                      {spot.subtitle}
-                    </h2>
-                    <div className="ml-4 mr-2 hidden rounded-2xl border-[.8px] border-solid px-3 py-1 text-sm text-[rgba(255,255,255,0.77)] md:inline">
-                      {spot.city}
-                    </div>
-                    <div className="ml-1 mr-2 hidden rounded-2xl border-[.8px] border-solid px-3 py-1 text-sm text-[rgba(255,255,255,0.77)] md:inline">
-                      {spot.spot_category}
+              <div key={index} className="relative">
+                <Link to={`/spot?id=${spot.id}`}>
+                  <div className="h-64 w-full overflow-hidden rounded-xl">
+                    <img
+                      src={spot.main_img}
+                      alt={spot.subtitle}
+                      className="h-full w-full transform object-cover transition-transform duration-500 ease-in-out hover:scale-110"
+                    />
+                    <div className="absolute bottom-0 left-0 mb-2 w-full rounded-xl bg-gradient-to-t from-slate-700/80 via-slate-700/40 to-transparent md:mb-0 md:h-2/6">
+                      <h2 className="z-10 mb-2 ml-4 mt-2 text-2xl text-white">
+                        {spot.subtitle}
+                      </h2>
                     </div>
                   </div>
+                </Link>
+                <div className="absolute bottom-0 left-2 mb-2 flex items-center space-x-2">
+                  <div className="ml-2 hidden cursor-pointer rounded-2xl border-[.8px] border-solid px-3 py-1 text-sm text-[rgba(255,255,255,0.77)] md:inline">
+                    {spot.city}
+                  </div>
+                  <div
+                    className="hidden cursor-pointer rounded-2xl border-[.8px] border-solid px-3 py-1 text-sm text-[rgba(255,255,255,0.77)] md:inline"
+                    onClick={() => {
+                      const component = components.find(
+                        (comp) => comp.spot_category === spot.spot_category,
+                      );
+                      if (component) {
+                        window.location.href = component.href;
+                      } else {
+                        console.error("找不到分類:", spot.spot_category);
+                      }
+                    }}
+                  >
+                    {spot.spot_category}
+                  </div>
                 </div>
-              </Link>
+              </div>
             ))}
           </div>
         </TabsContent>
         <TabsContent value="list" className="mt-3">
           <div className="flex flex-col gap-x-5 gap-y-4 md:grid md:grid-cols-2">
             {spots.map((spot, index) => (
-              <Link className="relative" key={index} to={`/spot?id=${spot.id}`}>
-                <div className="flex gap-3" key={index}>
+              <div key={index} className="relative flex gap-3">
+                <Link to={`/spot?id=${spot.id}`} className="flex-shrink-0">
                   <img
                     src={spot.main_img}
                     alt={spot.subtitle}
                     className="size-24 rounded-lg object-cover md:size-28"
                   />
-                  <div>
-                    <h2 className="text-xl">{spot.subtitle}</h2>
-                    <p className="mb-2 mt-1 text-base text-neutral-900">
-                      {spot.brief}
-                    </p>
-                    <div className="mr-2 hidden rounded bg-[rgba(209,250,229,0.6)] px-3 py-1 text-xs text-[#004B67] lg:inline xl:text-sm">
-                      {spot.city}
-                    </div>
-                    <div className="mr-2 hidden rounded bg-[rgba(209,250,229,0.6)] px-3 py-1 text-xs text-[#004B67] lg:inline xl:text-sm">
-                      {spot.spot_category}
-                    </div>
+                </Link>
+                <div className="max-w-[calc(100%-7rem)]">
+                  <h2 className="text-xl">{spot.subtitle}</h2>
+                  <p className="mb-2 mt-1 text-base text-neutral-900">
+                    {spot.brief}
+                  </p>
+                  <div className="mr-2 hidden rounded bg-[rgba(209,250,229,0.6)] px-3 py-1 text-xs text-[#004B67] lg:inline xl:text-sm">
+                    {spot.city}
+                  </div>
+                  <div
+                    className="mr-2 hidden cursor-pointer rounded bg-[rgba(209,250,229,0.6)] px-3 py-1 text-xs text-[#004B67] lg:inline xl:text-sm"
+                    onClick={() => {
+                      const component = components.find(
+                        (comp) => comp.spot_category === spot.spot_category,
+                      );
+                      if (component) {
+                        window.location.href = component.href;
+                      } else {
+                        console.error("找不到分類:", spot.spot_category);
+                      }
+                    }}
+                  >
+                    {spot.spot_category}
                   </div>
                 </div>
-              </Link>
+              </div>
             ))}
           </div>
         </TabsContent>
