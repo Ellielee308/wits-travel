6de472fa9d3719fe6a1866aff3ee613ab840ddde
chris <pupu86019@gmail.com>
2024-08-29 18:02:49 +0800

    feat: add UserTimeHeatmapBar component to visualize peak user interaction hours

---
+++ src/pages/StatisticsDashboard/UserTimeHeatmapBar.jsx
@@ -0,0 +1,74 @@
+import { Bar } from "react-chartjs-2";
+
+function UserTimeHeatmapBar({ usersData }) {
+  const interactionCounts = {};
+
+  usersData.forEach((user) => {
+    if (user.interactions) {
+      Object.values(user.interactions).forEach((interaction) => {
+        const timestamp = interaction.enter_timestamp;
+        const date = new Date(
+          timestamp.seconds * 1000 + timestamp.nanoseconds / 1000000,
+        );
+
+        const hour = date.getHours();
+
+        if (!interactionCounts[hour]) {
+          interactionCounts[hour] = 0;
+        }
+        interactionCounts[hour] += 1;
+      });
+    }
+  });
+
+  const chartData = {
+    labels: Object.keys(interactionCounts).map((hour) => `${hour}:00`),
+    datasets: [
+      {
+        label: "不同時段之點擊次數",
+        data: Object.values(interactionCounts),
+        backgroundColor: Object.values(interactionCounts).map((count) => {
+          if (count >= 0 && count <= 3) return "#E0e0e0";
+          if (count >= 4 && count <= 6) return "#aaae8e";
+          if (count >= 7) return "#607B7D";
+        }),
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: "時間點",
+        },
+      },
+      y: {
+        title: {
+          display: true,
+          text: "點擊次數",
+        },
+        beginAtZero: true,
+      },
+    },
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center rounded-md border border-gray-400 p-6">
+        <h1 className="text-2xl">熱門點擊時段</h1>
+        <div className="flex h-[300px] w-[1000px] justify-center">
+          <Bar data={chartData} options={options} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default UserTimeHeatmapBar;
