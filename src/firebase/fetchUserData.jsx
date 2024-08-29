b8ced372f61966cacb90d837fb6083ee3403f6d1
chris <pupu86019@gmail.com>
2024-08-29 17:51:37 +0800

    feat: fetch all documents from 'interactions' subcollection for each user
    - Modified  function to include retrieval of all documents from the 'interactions' subcollection for each user.

--- src/firebase/fetchUserData.jsx
+++ src/firebase/fetchUserData.jsx
@@ -4,7 +4,22 @@ import { collection, getDocs } from "firebase/firestore";
 export const fetchUserData = async () => {
   const usersRef = collection(db, "users");
   const usersSnap = await getDocs(usersRef);
-  const usersDataList = usersSnap.docs.map((doc) => doc.data());
+  const usersDataList = [];
+
+  for (const userDoc of usersSnap.docs) {
+    const userData = userDoc.data();
+    const userId = userDoc.id;
+
+    const interactionsRef = collection(db, "users", userId, "interactions");
+    const interactionsSnap = await getDocs(interactionsRef);
+    const interactionsData = interactionsSnap.docs.map((interactionDoc) =>
+      interactionDoc.data(),
+    );
+
+    userData.interactions = interactionsData;
+
+    usersDataList.push(userData);
+  }
 
   return usersDataList;
 };
