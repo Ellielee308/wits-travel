99b0d4c92b30874cce4da4eaa78224b861576f62
chris <pupu86019@gmail.com>
2024-08-29 11:28:40 +0800

    feat:  fetch users data from Firebase

--- src/pages/StatisticsDashboard/index.jsx
+++ src/pages/StatisticsDashboard/index.jsx
@@ -1,7 +1,27 @@
+import { fetchUserData } from "../../firebase/fetchUserData";
+import { useEffect, useState } from "react";
+import MemberNumberLineChart from "./MemberNumberLineChart";
+
 export default function StatisticsDashboard() {
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    const getUsersData = async () => {
+      try {
+        const usersDataList = await fetchUserData();
+        console.log("Fetched users data:", usersDataList);
+        setUsersData(usersDataList);
+      } catch (error) {
+        console.error("Error fetching users data:", error);
+      }
+    };
+    getUsersData();
+  }, []);
+
   return (
     <>
       <h1>這是圖表頁</h1>
+      <MemberNumberLineChart usersData={usersData} />
     </>
   );
 }
