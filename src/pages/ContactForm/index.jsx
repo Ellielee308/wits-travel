7c3d8b452953fe675d4c98b32ada061539e9347d
chris <pupu86019@gmail.com>
2024-08-30 02:12:19 +0800

    feat: Add subject field and replace static image with dynamically generated Carousel
    - Added a subject input field, allowing users to input the form subject.
    - Replaced the static travel image with a dynamically generated Carousel for displaying images.

--- src/pages/ContactForm/index.jsx
+++ src/pages/ContactForm/index.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { ErrorMessage } from "@hookform/error-message";
 import { submitContactForm } from "../../firebase/uploadForm";
+import CarouselInForm from "./CarouselInForm";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -24,8 +25,6 @@ import {
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
 
-import travelImg from "./travel.jpg";
-
 export default function ContactForm() {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const {
@@ -37,6 +36,7 @@ export default function ContactForm() {
     reset,
   } = useForm({
     defaultValues: {
+      purpose: "",
       name: "",
       email: "",
       phone: "",
@@ -70,6 +70,43 @@ export default function ContactForm() {
             旅遊行程安排/諮詢
           </h2>
           <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label className="mb-1 mt-5 block flex items-center text-lg font-medium lg:text-xl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="mr-2 h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
+                  />
+                </svg>
+                表單主旨
+              </label>
+
+              <Input
+                type="text"
+                className="mb-3 lg:text-lg"
+                placeholder="簡述您的問題"
+                {...register("purpose", { required: "主旨是必填項" })}
+              />
+
+              <ErrorMessage
+                errors={errors}
+                name="purpose"
+                render={({ message }) => (
+                  <p className="mt-1 text-xs text-red-600 lg:text-lg">
+                    {message}
+                  </p>
+                )}
+              />
+            </div>
+
             <div>
               <label className="mb-1 mt-5 block flex items-center text-lg font-medium lg:text-xl">
                 <svg
@@ -207,12 +244,12 @@ export default function ContactForm() {
                     d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
                   />
                 </svg>
-                主旨
+                詢問類別
               </label>
               <Controller
                 name="subject"
                 control={control}
-                rules={{ required: "請選擇主旨" }}
+                rules={{ required: "請選擇類別" }}
                 render={({ field }) => (
                   <Select
                     onValueChange={field.onChange}
@@ -220,7 +257,7 @@ export default function ContactForm() {
                     className="lg:w-72 lg:text-xl"
                   >
                     <SelectTrigger className="w-[180px] lg:w-72 lg:text-xl">
-                      <SelectValue placeholder="請選擇主旨" />
+                      <SelectValue placeholder="請選擇類別" />
                     </SelectTrigger>
                     <SelectContent>
                       <SelectItem value="問題詢問" className="lg:text-lg">
@@ -284,11 +321,7 @@ export default function ContactForm() {
           </form>
         </div>
         <div className="ml-20 mt-6 hidden h-full w-auto flex-shrink-0 items-center justify-center lg:mt-0 lg:flex lg:flex-1">
-          <img
-            src={travelImg}
-            alt="Travel"
-            className="h-[717px] w-[500px] object-cover"
-          />
+          <CarouselInForm />
         </div>
       </div>
       <div className="mb-5 mt-3 flex w-full justify-center lg:mb-10 lg:mt-8">
