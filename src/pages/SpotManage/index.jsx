5b0ee5aa68b68d0425f6e358566ae75138850abb
Ellie Lee <as152808@gmail.com>
2024-08-29 11:41:07 +0800

    fix: modify layout to fit navbar

--- src/pages/SpotManage/index.jsx
+++ src/pages/SpotManage/index.jsx
@@ -5,10 +5,10 @@ import AddForm from "./AddForm";
 export default function SpotManage() {
   const [showAddForm, setShowAddForm] = useState(false);
   return (
-    <div className="ml-48 mt-[60px] bg-white px-7 py-5 outline outline-1 outline-slate-600">
+    <div className="w-4/5 bg-white px-7 py-5">
       <div className="flex flex-row items-center justify-between">
-        <h1 className="select-none py-6 pl-6 text-2xl font-semibold text-gray-800">
-          {showAddForm ? "新增景點" : "景點管理"}
+        <h1 className="select-none py-6 text-2xl font-semibold text-gray-800">
+          {showAddForm ? "新增景點" : "編輯景點"}
         </h1>
         <button
           className="mb-4 mr-9 h-8 self-end rounded-lg bg-[#006c98] px-4 text-sm text-white transition-all duration-300"
