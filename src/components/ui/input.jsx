9d76d76483431a1ce18e8f3ad1b906d6d42f45d0
chris <pupu86019@gmail.com>
2024-09-02 11:56:10 +0800

    fix: Remove unnecessary props and update focus ring color

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,16 +1,14 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { cn } from "@/lib/utils";
+
 const Input = React.forwardRef(
-  ({ className, type = "text", hasError, ...props }, ref) => {
+  ({ className, type = "text", ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-stone-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-stone-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-stone-800 dark:bg-stone-950 dark:ring-offset-stone-950 dark:placeholder:text-stone-400",
-          hasError
-            ? "focus-visible:ring-red-600"
-            : "focus-visible:ring-stone-950",
+          "flex h-10 w-full rounded-md border border-stone-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-stone-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-stone-300 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-stone-800 dark:bg-stone-950 dark:ring-offset-stone-950 dark:placeholder:text-stone-400 dark:focus-visible:ring-stone-300",
           className,
         )}
         ref={ref}
@@ -25,7 +23,6 @@ Input.displayName = "Input";
 Input.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
-  hasError: PropTypes.bool,
 };
 
 export { Input };
