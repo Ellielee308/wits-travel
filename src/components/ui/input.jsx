512f7a93b46339023b9e8f495854ee75a17d2aa6
chris <pupu86019@gmail.com>
2024-09-02 09:27:53 +0800

    feat: Add hasError prop to Input component for error state styling

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,14 +1,16 @@
 import * as React from "react";
 import PropTypes from "prop-types";
 import { cn } from "@/lib/utils";
-
 const Input = React.forwardRef(
-  ({ className, type = "text", ...props }, ref) => {
+  ({ className, type = "text", hasError, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-stone-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-stone-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-stone-950 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-stone-800 dark:bg-stone-950 dark:ring-offset-stone-950 dark:placeholder:text-stone-400 dark:focus-visible:ring-stone-300",
+          "flex h-10 w-full rounded-md border border-stone-200 bg-white px-3 py-2 text-sm ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-stone-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-stone-800 dark:bg-stone-950 dark:ring-offset-stone-950 dark:placeholder:text-stone-400",
+          hasError
+            ? "focus-visible:ring-red-600"
+            : "focus-visible:ring-stone-950",
           className,
         )}
         ref={ref}
@@ -23,6 +25,7 @@ Input.displayName = "Input";
 Input.propTypes = {
   className: PropTypes.string,
   type: PropTypes.string,
+  hasError: PropTypes.bool,
 };
 
 export { Input };
